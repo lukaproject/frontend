bfb00c3d3bb06565cc7d9640b888f4aa6386b646
Dxyinme <32793868+dxyinme@users.noreply.github.com>
2025-06-11 21:16:49 +0800

    Dxyinme/dev (#5)
    * tasks finished
    
    * move react-script to vite
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/views/Tasks/SingleTask.tsx
+++ src/views/Tasks/SingleTask.tsx
@@ -1,10 +1,26 @@
-import { Flex, Layout } from "antd";
+import { Card, Flex, Layout, Space } from "antd";
 import React from "react";
 import LukaFooter from "../commons/footer";
 import { LukaHeaderStyle } from "../css/commoncss";
 import { useParams } from "react-router-dom";
 import LukaBreadcrumb from "../commons/Breadcrumb";
 const { Content, Header } = Layout;
+
+function TaskInfo(taskInfo: SingleTaskViewProp.TaskInfoProps) {
+    return (
+        <Space direction="vertical" size={16}>
+            <Card title="base info" style={{ width: 300 }}>
+                <p>Task Display Name: {taskInfo.Name}</p>
+                <p>Create Time: {taskInfo.CreateTime!.toString()}</p>
+                <p>Finished Time: {taskInfo.FinishedTime!.toString()}</p>
+                <p>ExitCode: {taskInfo.ExitCode!.toString()}</p>
+            </Card>
+            <Card title="script" style={{ width: 300 }}>
+                {taskInfo.Script}
+            </Card>
+        </Space>
+    )
+}
 
 function SingleTask() {
     const { bagDisplayName, taskDisplayName } = useParams()
@@ -22,9 +38,13 @@ function SingleTask() {
             ]
             }></LukaBreadcrumb >
             <Content style={{ padding: '0 48px' }}>
-                <div>
-                    Single Task
-                </div>
+                <TaskInfo
+                    Name={taskDisplayName}
+                    CreateTime={new Date(2024, 10, 1)}
+                    FinishedTime={new Date(Date.now())}
+                    ExitCode={10}
+                    Script="echo 1"
+                />
             </Content>
             <LukaFooter />
         </Flex >
